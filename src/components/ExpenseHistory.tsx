53c6a7352a3b2cdabff0d72c4f374ff4f33569e1
Dobri Dobrevski <53184157+DobriJS@users.noreply.github.com>
2022-09-19 13:59:19 +0300

    Map expenses history

--- src/components/ExpenseHistory.tsx
+++ src/components/ExpenseHistory.tsx
@@ -1,5 +1,17 @@
+import { useAppSelector } from '../hooks/hooks';
+
 const ExpenseHistory = () => {
-  return <div>ExpenseHistory</div>;
+  const expenses = useAppSelector((state) => state.expense.expenseHistory);
+  return (
+    <div style={{ width: '100%' }}>
+      {expenses.map((ex, i) => (
+        <div key={i}>
+          <div>{ex.expenseName}</div>
+          <div>{ex.amount}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ExpenseHistory;
