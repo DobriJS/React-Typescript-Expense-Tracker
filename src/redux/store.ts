e8f3b4c0fadf0c80316b543a62b0799ae20de801
Dobri Dobrevski <53184157+DobriJS@users.noreply.github.com>
2022-09-19 13:53:34 +0300

    Add expense reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import expenseReducer from './expenseSlice';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    expense: expenseReducer
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
